876a8a0c9c2644cbfc6916ba7dcbc091c0337892
shachs <shachs@stam>
2020-12-22 11:34:35 +0200

    redetecter first commit

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,10 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 from .detr import build
+from .redetectr import build as build_redetectr
 
 
 def build_model(args):
     return build(args)
+
+def build_redetectr_model(args):
+    return build_redetectr(args)
